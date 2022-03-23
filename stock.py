79438ee8622bf3900f2faead189f6290a50b6275
Baron <nm6101080@gs.ncku.edu.tw>
2022-03-23 14:21:21 +0800

    version 0

--- stock.py
+++ stock.py
@@ -1,9 +1,11 @@
 import requests 
 import json
 import torch
+import wandb
 import numpy as np
 from torch import nn
 from torch.utils.data import Dataset, DataLoader
+from sklearn.model_selection import train_test_split
 
 # stock info
 u_id = "NM6101080"
@@ -13,9 +15,11 @@ vol = 5
 price = 590
 
 # Model para
+learning_rate = 0.0001
+EPOCH = 10000
 
 
-class TrainSet(Dataset):
+class MydataSet(Dataset):
     def __init__(self, data):
         self.data, self.label = data[:, :-1].float(), data[:, -1].float()
 
@@ -42,28 +46,64 @@ class RNN(nn.Module):
         r_out, (h_n, h_c) = self.rnn(x, None)  # None 表示 hidden state 會用全0的 state
         out = self.out(r_out)
         return out
+
+class ReadData():
+    def __init__(self, stock, s_date, e_date):
+        api_url = "http://140.116.86.242:8081/stock/api/v1/api_get_stock_info_from_date_json/{}/{}/{}".format(stock, s_date, e_date)
+        r = requests.get(api_url)
+        history_info = json.loads(r.text) 
+        # data rerset low_price to next day
+        target = [data["low"] for i, data in enumerate(history_info) if i != 0]
+        history_data = [
+            [data['capacity'], data['turnover'], data["open"], data["high"],data["close"],data["change"],data["transaction_volume"], target[i]] 
+            for i, data in enumerate(history_info) if i != len(history_info)-1
+        ]
+        history_data = np.array(history_data)
+        self.__training_data, self.__testing_data = train_test_split(history_data, test_size=0.2)
         
-def read_data(stock, s_date, e_date):
-    api_url = "http://140.116.86.242:8081/stock/api/v1/api_get_stock_info_from_date_json/{}/{}/{}".format(stock, s_date, e_date)
-    r = requests.get(api_url)
-    history_info = json.loads(r.text) 
-    # data rerset low_price to next day
-    target = [data["low"] for i, data in enumerate(history_info) if i != 0]
-    history_training = [
-        [data['capacity'], data['turnover'], data["open"], data["high"],data["close"],data["change"],data["transaction_volume"], target[i]] 
-        for i, data in enumerate(history_info) if i != len(history_info)-1
-    ]
-    # normalize x: input type "numpy array"
-    history_array = np.array(history_training)
-    y = history_array[:, -1].reshape(-1, 1)
-    nor_x = normalize(history_array[:, :-1])
+    @property
+    def get_training_data(self):
+        # normalize x: input type "numpy array"
+        y = self.__training_data[:, -1].reshape(-1, 1)
+        nor_x = self.normalize(self.__training_data[:, :-1])
 
-    return torch.Tensor(np.concatenate((nor_x, y), axis=1))
+        return torch.Tensor(np.concatenate((nor_x, y), axis=1))
+    
+    @property
+    def get_testing_data(self):
+        # normalize x: input type "numpy array"
+        y = self.__testing_data[:, -1].reshape(-1, 1)
+        nor_x = self.normalize(self.__testing_data[:, :-1])
 
-def normalize(data):
-    mean = np.mean(data, axis=0)
-    std = np.std(data, axis=0)
-    return  (data - mean) / std
+        return torch.Tensor(nor_x), y
+
+    def normalize(self, data):
+        mean = np.mean(data, axis=0)
+        std = np.std(data, axis=0)
+        return  (data - mean) / std
+
+def train(trainloader, feature_number):
+    model = RNN(feature_number)
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)  # optimize all cnn parameters
+    loss_func = nn.MSELoss()
+    for step in range(EPOCH):
+        for tx, ty in trainloader:
+            output = model(torch.unsqueeze(tx, dim=0))
+            loss = loss_func(torch.squeeze(output), ty)
+            optimizer.zero_grad()  # clear gradients for this training step
+            loss.backward()  # back propagation, compute gradients
+            wandb.log({"training_loss": loss}, step=step)
+            optimizer.step()
+        if step % 500 ==0:
+            print("[{}/{}] Loss:{:.4f}".format(step, EPOCH, loss.item()))
+
+    torch.save(model.state_dict(), "./models/save_model.pth")
+    return model
+
+def predict(model, test_x, test_y):
+    predict_y = model(test_x)
+    print(predict_y)
+    print(test_y)
 
 def main():
     r = requests.post("http://140.116.86.242:8081/stock/api/v1/buy", data={"uname":u_id, "pass":password, "scode": scode, "svol": str(vol), "sell_price":str(price)})
@@ -71,10 +111,13 @@ def main():
 
 
 if __name__ == "__main__":
-    test_data = read_data(2330, 20150301, 20220322)
-    print(test_data.shape)
-    print(test_data[0])
-    train = TrainSet(test_data)
-    print(train)
-    mydataloader = DataLoader(dataset=train, batch_size=64)
-
+    wandb.init(project='Stock', entity="baron")
+    all_data = ReadData(2330, 20150301, 20220322)
+    training_data = all_data.get_training_data
+    train_data = MydataSet(training_data)
+    mydataloader = DataLoader(dataset=train_data, batch_size=64)
+    model = train(mydataloader, training_data.shape[1]-1)
+    test_x, test_y = all_data.get_testing_data
+    predict(model, test_x, test_y)
+    wandb.finish()
+    
